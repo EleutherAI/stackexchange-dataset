170545fd9dddb39685113d503094b4a5ab43905b
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2023-11-06 20:39:20 +0900

    cosmetics

--- main.py
+++ main.py
@@ -41,10 +41,6 @@ def download_and_process_single(name, out_format, min_score, max_responses, keep
                 s.download()
                 # s.remove_dump()
 
-        if not os.path.isfile(path_to_xml):
-            # extract 7z if it's not extracted already
-            s.extract()
-
         if out_format == LM_DATAFORMAT_FORMAT:
             archiver = Archive(out_folder)
         elif out_format == TEXT_FORMAT:
@@ -53,6 +49,10 @@ def download_and_process_single(name, out_format, min_score, max_responses, keep
             archiver = JSONArchive(out_folder)
         else:
             archiver = None
+
+        if not os.path.isfile(path_to_xml):
+            # extract 7z if it's not extracted already
+            s.extract()
 
         qa = QA_Pairer(path_to_xml, name=name, out_format=out_format, archiver=archiver, min_score=min_score,
                        max_responses=max_responses)
